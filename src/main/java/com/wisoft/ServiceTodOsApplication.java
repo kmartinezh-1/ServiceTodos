e6b2e4c72bf17c355a6eba8c53a80998ce7af991
kmartinezh-1 <kamar.ing@hotmail.com>
2021-04-17 10:27:15 -0500

    Practica rest 1

---
+++ src/main/java/com/wisoft/ServiceTodOsApplication.java
@@ -0,0 +1,13 @@
+package com.wisoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceTodOsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceTodOsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wisoft/ServiceTodOsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wisoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceTodOsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
